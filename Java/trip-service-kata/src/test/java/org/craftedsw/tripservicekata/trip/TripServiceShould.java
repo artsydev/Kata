68e68435f74a7a9fdb2897eb56c06d6ec4a8ce86
Shane Thomas <shane120@gmail.com>
2017-12-08 00:32:37 -0700

    Add test for TripService
    Was unable to run test because of the Singleton call to UserSession in
    the getTripsByUser() method.
    
    To get around this, I created a new protected method called
    getLoggedUser() which I then overrode in the test in order to eliminate
    the Singleton from the test.

--- Java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ Java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,8 +1,29 @@
 package org.craftedsw.tripservicekata.trip;
 
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class TripServiceShould {
 
+    @Test public void
+    throw_exception_if_user_is_not_logged_in() {
+        TripServiceTest trip = new TripServiceTest();
+        User guest = new User();
 
+        assertThrows(UserNotLoggedInException.class, () -> trip.getTripsByUser(guest));
+    }
+
+    class TripServiceTest extends TripService {
+
+        @Override protected User getLoggedUser() {
+            return null;
+        }
+
+    }
 
 }
