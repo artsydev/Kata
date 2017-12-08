15feaddf2d4d201ef7fef7649280c47e9ba590f1
Shane Thomas <shane120@gmail.com>
2017-12-08 15:03:57 -0700

    Start refactoring getTripsByUser()
    We can start refactoring safely now that we have passing tests for the
    method.
    
    * Inverted if() logic to expose the guard clause
    * Removed the method logic from the if/else so that the guard clause
    stands alone and the rest of the logic is clearer now that it is not
    mixed together

--- Java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ Java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -12,30 +12,33 @@ import java.util.List;
 public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<Trip>();
+        List<Trip> tripList = new ArrayList<>();
         User loggedUser = loggedInUser();
-        boolean isFriend = false;
-        if (loggedUser != null) {
-            for (User friend : user.getFriends()) {
-                if (friend.equals(loggedUser)) {
-                    isFriend = true;
-                    break;
-                }
-            }
-            if (isFriend) {
-                tripList = tripsByUser(user);
-            }
-            return tripList;
-        } else {
+
+        if (loggedUser == null) {
             throw new UserNotLoggedInException();
         }
+
+        boolean isFriend = false;
+        for (User friend : user.getFriends()) {
+            if (friend.equals(loggedUser)) {
+                isFriend = true;
+                break;
+            }
+        }
+        if (isFriend) {
+            tripList = tripsByUser(user);
+        }
+        return tripList;
     }
 
     protected User loggedInUser() {
-        return UserSession.getInstance().getLoggedUser();
+        return UserSession.getInstance()
+                          .getLoggedUser();
     }
 
     protected List<Trip> tripsByUser(User user) {
         return TripDAO.findTripsByUser(user);
     }
+
 }
