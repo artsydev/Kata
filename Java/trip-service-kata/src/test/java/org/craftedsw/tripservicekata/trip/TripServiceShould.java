a2866ce02b1a2cce469f4ffaf99bed56d2d911f0
Shane Thomas <shane120@gmail.com>
2017-12-08 03:29:59 -0700

    Refactored common init code into the @BeforeEach method

--- Java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ Java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -3,11 +3,12 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -19,12 +20,19 @@ public class TripServiceShould {
     private static final User REGISTERED_USER = new User();
     private static final User SOMEBODY_ELSE = new User();
     private static final Trip PHILADELPHIA = new Trip();
+    private static final Trip CALIFORNIA = new Trip();
+    private static final Trip GEORGIA = new Trip();
+    private TripService tripService;
     private User loggedInUser;
+
+    @BeforeEach
+    public void initialise() {
+        tripService = new TestableTripService();
+        loggedInUser = REGISTERED_USER;
+    }
 
     @Test public void
     validate_that_the_user_is_logged_in() {
-        TestableTripService tripService = new TestableTripService();
-
         loggedInUser = GUEST;
 
         assertThrows(UserNotLoggedInException.class,
@@ -33,10 +41,6 @@ public class TripServiceShould {
 
     @Test public void
     not_return_any_trips_when_users_are_not_friends() {
-        TestableTripService tripService = new TestableTripService();
-
-        loggedInUser = REGISTERED_USER;
-
         User stranger = new User();
         stranger.addFriend(SOMEBODY_ELSE);
         stranger.addTrip(PHILADELPHIA);
@@ -48,18 +52,16 @@ public class TripServiceShould {
 
     @Test public void
     return_trips_when_users_are_friends() {
-        TestableTripService tripService = new TestableTripService();
-
-        loggedInUser = REGISTERED_USER;
-
         User friend = new User();
         friend.addFriend(loggedInUser);
         friend.addFriend(SOMEBODY_ELSE);
         friend.addTrip(PHILADELPHIA);
+        friend.addTrip(GEORGIA);
+        friend.addTrip(CALIFORNIA);
 
         List<Trip> trips = tripService.getTripsByUser(friend);
 
-        assertThat(trips.size(), is(1));
+        assertThat(trips.size(), is(3));
     }
 
 
