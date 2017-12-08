010cc86e3a43fb2b8f5e1f82e8afc4935cfd6e17
Shane Thomas <shane120@gmail.com>
2017-12-08 02:47:26 -0700

    Added test for when users are not friends
    Continued refactoring test code to make domain clearer.

--- Java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ Java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -3,23 +3,48 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class TripServiceShould {
 
     private static final User GUEST = null;
+    private static final User REGISTERED_USER = new User();
+    private static final User SOMEBODY_ELSE = new User();
+    private static final Trip SPOKANE = new Trip();
     private User loggedInUser;
 
     @Test public void
     validate_that_the_user_is_logged_in() {
-        TestableTripService trip = new TestableTripService();
+        TestableTripService tripService = new TestableTripService();
+
         loggedInUser = GUEST;
 
-        assertThrows(UserNotLoggedInException.class, () -> trip.getTripsByUser(loggedInUser));
+        assertThrows(UserNotLoggedInException.class,
+                     () -> tripService.getTripsByUser(loggedInUser));
     }
+
+    @Test public void
+    not_return_any_trips_when_users_are_not_friends() {
+        TestableTripService tripService = new TestableTripService();
+
+        loggedInUser = REGISTERED_USER;
+
+        User stranger = new User();
+        stranger.addFriend(SOMEBODY_ELSE);
+        stranger.addTrip(SPOKANE);
+
+        List<Trip> trips = tripService.getTripsByUser(stranger);
+
+        assertThat(trips.size(), is(0));
+    }
+
 
     class TestableTripService extends TripService {
 
